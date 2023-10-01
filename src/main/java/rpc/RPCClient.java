dda8f80c1dd869cbff7d07d203fee4c75dc168d3
Ryan Nóbrega Brandão <ryanbrandao18@gmail.com>
2023-09-30 23:06:42 -0300

    atividade rabbitmq rpc

--- src/main/java/rpc/RPCClient.java
+++ src/main/java/rpc/RPCClient.java
@@ -27,7 +27,7 @@ public class RPCClient implements AutoCloseable {
         try (RPCClient fibonacciRpc = new RPCClient()) {
             for (int i = 0; i < 32; i++) {
                 String i_str = Integer.toString(i);
-                System.out.println(" [x] Requesting fib Renata Albuquerque(" + i_str + ")");
+                System.out.println(" [x] Requesting fib Ryan Nóbrega Brandão(" + i_str + ")");
                 String response = fibonacciRpc.call(i_str);
                 System.out.println(" [.] Got '" + response + "'");
             }
